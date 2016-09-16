9dd3ba18774df472fb682644e12b94ade0bbca1c
luongquocdinh <luongquocdinh.95@gmail.com>
2016-09-16 23:43:09 +0700

    react components

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-// In webpack.config.js
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/app/index.html',
   filename: 'index.html',
   inject: 'body'
 });
+
 module.exports = {
   entry: [
     './app/index.js'
