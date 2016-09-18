4ef562eadcf12dee97295f70c58de8dcb815de4f
luongquocdinh <luongquocdinh.95@gmail.com>
2016-09-18 12:34:41 +0700

    this.props.children and getting started with React Router

--- app/index.js
+++ app/index.js
@@ -1,49 +1,8 @@
-var USER_DATA = {
-  name: 'Luong Quoc Dinh',
-  username: 'luongquocdinh',
-  images: 'https://avatars3.githubusercontent.com/u/7899186?v=3&s=466'
-}
-
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var ProfilePic = React.createClass({
-  render: function () {
-    return <img src={this.props.imageUrl} style={{height: 200, width: 200}}></img>
-  }
-});
-
-var ProfileLink = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <a href={'https://www.github.com/' + this.props.username}>
-          {this.props.username}
-        </a>
-      </div>
-    )
-  }
-});
-
-var ProfileName = React.createClass({
-  render: function () {
-    return <div>{this.props.name}</div>
-  }
-})
-
-var Avatar = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <ProfilePic imageUrl={this.props.user.images} />
-        <ProfileName name={this.props.user.name} />
-        <ProfileLink username={this.props.user.username} />
-      </div>
-    );
-  }
-});
+var routes = require('./config/routes');
 
 ReactDOM.render(
-  <Avatar user={USER_DATA} />,
+  routes,
   document.getElementById('app')
 );
