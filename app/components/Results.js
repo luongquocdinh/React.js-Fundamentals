e86dc9db214329e67b5ef09270f260b458ec9bd1
luongquocdinh <luongquocdinh.95@gmail.com>
2016-09-23 15:25:37 +0700

    Building a Highly Reusable React Component

--- app/components/Results.js
+++ app/components/Results.js
@@ -6,6 +6,7 @@ var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 var Link = ReactRouter.Link;
 var MainContainer = require('./MainContainer');
+var Loading = require('./Loading');
 
 function StartOver () {
   return (
@@ -18,6 +19,10 @@ function StartOver () {
 }
 
 function Results (props) {
+  if (props.isLoading === true) {
+    return <Loading text={'One Moment'} speed={100}/>
+  }
+
   if (props.scores[0] === props.scores[1]) {
     return (
       <MainContainer>
